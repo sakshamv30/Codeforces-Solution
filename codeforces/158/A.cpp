aa4ee1cb9b0ade03c26664c73c1a4d46b513e16c
Saksham Varshney <sakshamv30@gmail.com>
2018-09-22 23:08:00 +0530

    Add solution for problem `A - Next Round` Link: https://codeforces.com/contest/158/problem/A Tags: *special problem, implementation, *800 Ref: https://codeforces.com/contest/158/submission/43268419

---
+++ codeforces/158/A.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main()
+{
+   
+ int n,k,count=0;
+   
+ scanf("%d%d",&n,&k);
+   
+ int num[n];
+  
+  for(int i=0; i<n; i++)
+    
+    scanf("%d",&num[i]);
+   
+ if(num[k-1]==0)
+    {
+    
+    for(int i=0; i<n; i++)
+       
+ {
+    
+        if(num[i]>num[k-1])
+       
+         count++;
+        }
+    }
+ 
+   else
+    {
+     
+   for(int i=0; i<n; i++)
+ 
+       {
+           
+ if(num[i]>=num[k-1])
+    
+            count++;
+  
+      }
+    }
+    
+printf("%d\n",count);
+    return 0;
+}
+
+
+
+
