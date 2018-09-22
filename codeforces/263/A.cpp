4f3afc8b08726af9b069e4b1acd6ea30852d04f1
Saksham Varshney <sakshamv30@gmail.com>
2018-09-23 00:03:00 +0530

    Add solution for problem `A - Beautiful Matrix` Link: https://codeforces.com/contest/263/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/263/submission/43270257

--- codeforces/263/A.cpp
+++ codeforces/263/A.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main() {
-	int x=0,y=0;
-	 
-	 for(int i=0;i<5;i++)
-	 {
-	 	
-	 for(int j=0;j<5;j++)
-	 {	
-	 	int a;
-	 	cin>>a;
-	 	if(a==1){ x=i+1; y=j+1 ;}
-	 	
-	 }
-	 }
-	 cout<<abs(3-x)+abs(3-y)<<endl;
-	
-	
-	
-	return 0;
+int main()
+{
+    int x = 0;
+    for (int i = 1; i <= 5; ++i)
+    {
+        for (int j = 1; j <= 5; ++j)
+        {
+            cin >> x;
+            if (x == 1)
+            {
+                cout << abs(i - 3) + abs(j - 3) << endl;
+            }
+        }
+    }
+    return 0;
 }
