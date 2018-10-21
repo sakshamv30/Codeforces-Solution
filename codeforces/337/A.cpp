6c9bc101201c3515a147b431ae391849646eeca1
Saksham Varshney <sakshamv30@gmail.com>
2018-10-21 13:09:00 +0530

    Add solution for problem `A - Puzzles` Link: https://codeforces.com/contest/337/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/337/submission/44623142

---
+++ codeforces/337/A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int maxn=50+4;
+int a[maxn];
+
+int main()
+{
+    int n,m;
+    while(cin>>n>>m)
+    {
+        for(int i=0;i<m;i++)
+            cin>>a[i];
+        sort(a,a+m);
+
+        int ans=a[n-1]-a[0];
+
+        for(int i=1;i<=m-n;i++)
+        {
+            ans=min(a[i+n-1]-a[i],ans);
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
