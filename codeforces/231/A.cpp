cfad1330f15e39f8bb504bc5e2929d0d12bb5869
Saksham Varshney <sakshamv30@gmail.com>
2020-07-04 16:27:00 +0530

    Add solution for problem `A - Team` Link: https://codeforces.com/contest/231/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/231/submission/85915521

---
+++ codeforces/231/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int co=0;
+	while(n--)
+	{	int a,b,c;
+	
+	cin>>a>>b>>c;
+	int sum=a+b+c;
+	if(sum==2 || sum==3) co++;
+	
+		
+		
+		
+		
+	}
+	cout<<co<<endl;
+	return 0;
+}
